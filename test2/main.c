11677ea7b8bf96c84f8727099ef45f78991fbdf1
silas-inegbe <kingssilas18@gmail.com>
2022-12-20 18:09:09 +0100

    done

---
+++ test2/main.c
@@ -0,0 +1,19 @@
+#include "shell.h"
+/**
+ *  main - Entry point for shell, handles args to shell
+ *  @ac: Arg count
+ *  @av: Array of args
+ *  @env: Environment
+ *  Return: 0;
+ */
+int main(int ac, char **av, char **env)
+{
+	if (!ac)
+		(void)ac;
+	if (!av)
+		(void)av;
+	if (!env)
+		(void)env;
+	shell(ac, av, env);
+	return (0);
+}
